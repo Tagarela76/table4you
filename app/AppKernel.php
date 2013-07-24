f13d4501ebee32c27556979a5f23ac038b741dce
Masha Andreeva <masha.an@kttsoft.com>
2013-07-24 14:12:50 +0300

    add captcha bundle, change admin link , start to configure register form

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,6 +29,7 @@ class AppKernel extends Kernel
             new Application\Sonata\AdminBundle\ApplicationSonataAdminBundle(),
             new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
             new Table\MainBundle\TableMainBundle(),
+            new Gregwar\CaptchaBundle\GregwarCaptchaBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Application/Sonata/UserBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Application\Sonata\UserBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+
+class RegistrationFormType extends BaseType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder
+            ->add('captcha', 'captcha')
+        ;
+    }
+
+    public function getName()
+    {
+        return 'table_user_registration';
+    }
+}
