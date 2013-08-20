8b234261236751f7a02ce2fe54040a536a376b47
Masha Andreeva <masha.an@kttsoft.com>
2013-08-21 00:55:30 +0300

    profile show

---
+++ src/Application/Sonata/UserBundle/Form/Type/ProfileType.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+namespace Application\Sonata\UserBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+use Sonata\UserBundle\Form\Type\ProfileType as BaseProfileType;
+
+class ProfileType extends BaseProfileType
+{
+    private $class;
+
+    /**
+     * @param string $class The User class name
+     */
+    public function __construct($class)
+    {
+        $this->class = $class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', null, array(
+                'label' => 'profile.username.form',
+                'translation_domain' => 'SonataUserBundle'
+            ))
+            ->add('lastname', null, array(
+                'label' => 'profile.lastname.form', 
+                'translation_domain' => 'SonataUserBundle'
+            ))
+            ->add('email', 'email', array(
+                'label' => 'profile.email.form', 
+                'translation_domain' => 'SonataUserBundle'
+            ))
+            ->add('password', 'repeated', array(
+                'type' => 'password',
+                'invalid_message' => 'Password fields do not match',
+                'first_options' => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+            ))
+            ->add('phone', 'text', array(
+                'label' => 'profile.phone.form',
+                'translation_domain' => 'SonataUserBundle',
+                'required' => true,
+            ))    
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => $this->class,
+            'intention'  => 'profile',
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'sonata_user_profile';
+    }
+}
