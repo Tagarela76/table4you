7bc25864f7a7496fc05edad16380d883bbac4073
Masha Andreeva <masha.an@kttsoft.com>
2014-03-20 12:28:45 +0200

    fix issue with files

--- src/Table/RestaurantBundle/Resources/public/js/common.js
+++ src/Table/RestaurantBundle/Resources/public/js/common.js
@@ -615,8 +615,12 @@ function Common() {
                     " " + numberOfPlaces + "</div>" +
                     "<div class='fileinput fileinput-new' data-provides='fileinput'>"+
                     "<div class='input-group'>"+
-                        "<div class='form-control files-download' data-trigger='fileinput'><i class='glyphicon glyphicon-file fileinput-exists'></i> <span class='fileinput-filename'></span></div>"+
-                        "<span class='input-group-addon btn btn-default btn-file'><span class='fileinput-new'>" + selectTheFileLabel + "</span><input type='file' name='...'></span>"+
+                        "<div class='form-control files-download' data-trigger='fileinput'>" +
+                        "<i class='glyphicon glyphicon-file fileinput-exists'></i> " +
+                        "<span class='fileinput-filename'></span></div>"+
+                        "<span class='input-group-addon btn btn-default btn-file'>" +
+                        "<span class='fileinput-new'>" + selectTheFileLabel + "</span>" +
+                        "<input type='file' name='file[]' size='1'></span>"+
                         "</div>"+
                     "</span></div>"+
                     "<a href='#' onclick='page.common.removeFileField(this); return false;' id='delete-file-customization'> " +
